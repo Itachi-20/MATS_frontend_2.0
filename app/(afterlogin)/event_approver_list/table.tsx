dcf5b201f95c19a2074705da54d562de159e048c
Harsh Jain <149000742+MLS111@users.noreply.github.com>
2024-12-10 12:00:16 +0530

    event approver list and advance listing

--- app/(afterlogin)/event_approver_list/table.tsx
+++ app/(afterlogin)/event_approver_list/table.tsx
@@ -55,6 +55,44 @@ const table = ({ ...Props }: Props) => {
               <SelectItem value="system">System</SelectItem>
             </SelectContent>
           </Select>
+          <Button className="text-black text-md font-normal bg-white hover:bg-white border rounded-[25px] px-8 py-5 shadow">
+            Back
+          </Button>
+          <div className="">
+            <svg
+              width="45"
+              height="45"
+              viewBox="0 0 50 50"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <g id="Group 1707480146">
+                <path
+                  id="Vector"
+                  d="M25 0C21.717 0 18.4661 0.646644 15.4329 1.90301C12.3998 3.15938 9.6438 5.00087 7.32233 7.32233C2.63392 12.0107 0 18.3696 0 25C0 31.6304 2.63392 37.9893 7.32233 42.6777C9.6438 44.9991 12.3998 46.8406 15.4329 48.097C18.4661 49.3534 21.717 50 25 50C31.6304 50 37.9893 47.3661 42.6777 42.6777C47.3661 37.9893 50 31.6304 50 25C50 21.717 49.3534 18.4661 48.097 15.4329C46.8406 12.3998 44.9991 9.6438 42.6777 7.32233C40.3562 5.00087 37.6002 3.15938 34.5671 1.90301C31.5339 0.646644 28.283 0 25 0Z"
+                  fill="#ECF2FF"
+                />
+                <rect
+                  id="Rectangle 3959"
+                  x="22"
+                  y="22"
+                  width="6"
+                  height="16"
+                  rx="2"
+                  fill="#4430BF"
+                />
+                <rect
+                  id="Rectangle 3960"
+                  x="22"
+                  y="12"
+                  width="6"
+                  height="6"
+                  rx="3"
+                  fill="#4430BF"
+                />
+              </g>
+            </svg>
+          </div>
         </div>
       </div>
       <div className="border bg-white  p-4 rounded-[18px]">
@@ -104,6 +142,13 @@ const table = ({ ...Props }: Props) => {
                 }
               >
                 Event Requestor
+              </TableHead>
+              <TableHead
+                className={
+                  "text-center  text-[#625d5d] text-[15px] font-normal font-['Montserrat']"
+                }
+              >
+                Created By
               </TableHead>
               <TableHead
                 className={
@@ -166,12 +211,13 @@ const table = ({ ...Props }: Props) => {
               tableData.map((data: any, index: number) => {
                 return (
                   <TableRow key={index} className="text-center text-nowrap">
-                    <TableCell>{data.name}</TableCell>
-                    <TableCell>{data.event_name}</TableCell>
-                    <TableCell> {data.event_type}</TableCell>
-                    <TableCell>{data.event_start_date}</TableCell>
+                    <TableCell>{data.name ?? "-"}</TableCell>
+                    <TableCell>{data.event_name ?? "-"}</TableCell>
+                    <TableCell> {data.event_type ?? "-"}</TableCell>
+                    <TableCell>{data.event_start_date ?? "-"}</TableCell>
                     <TableCell>{data.total_estimated_expense}</TableCell>
-                    <TableCell>{data.event_requestor}</TableCell>
+                    <TableCell>{data.event_requestor ?? "-"}</TableCell>
+                    <TableCell>{data.owner ?? "-"}</TableCell>
                     <TableCell>
                       <div className="flex flex-col items-center">
                         {data.level1}
