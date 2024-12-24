4e3db411d37ef69482aff56d10f9fe18988bc776
Itachi-20 <neel.udvadia@merillife.com>
2024-12-24 17:27:17 +0530

    added new state route

---
+++ app/api/fetchState/route.ts
@@ -0,0 +1,24 @@
+import { NextResponse } from 'next/server';
+
+export async function GET(req: Request) {
+    try {
+        const cookies = req.headers.get("cookie")
+        const response = await fetch(`${process.env.FRAPPE_URL}/api/resource/City List`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'Cookie': cookies as string 
+            },
+            credentials:'include'
+        });
+
+        if (!response.ok) {
+            throw new Error(`Error: ${response.statusText}`);
+        }
+
+        
+        return response;
+    } catch (error: any) {
+        return NextResponse.json({ error: error.message || 'Something went wrong' }, { status: 500 });
+    }
+}
